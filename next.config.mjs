90da8660d2d7f40ded22c672ae2cdafa519cade1
Manish Patil <155523680+manishpatil55@users.noreply.github.com>
2025-03-24 01:45:23 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Add this line for static export
+
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
