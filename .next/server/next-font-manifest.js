759cb7de910f8818e851702f7ca9be3234bad5e5
Manish Patil <155523680+manishpatil55@users.noreply.github.com>
2025-07-14 17:54:29 +0530

    Clean up tracked .next and .DS_Store files

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/manishpatil/Documents/GitHub/manishpatil55.github.io/src/app/layout\":[\"static/media/c2e245741bb6adef-s.p.woff2\",\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
